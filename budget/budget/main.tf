5091d77f046900b1f56ee8baa16ad31ea3dfc99f
Mateusz Umiński <17390661+mateusz-uminski@users.noreply.github.com>
2022-11-06 18:31:48 +0100

    feat: add module that creates aws budget

---
+++ budget/budget/main.tf
@@ -0,0 +1,17 @@
+resource "aws_budgets_budget" "monthly_budget" {
+  name              = "monthly-learning-budget-${var.budget_limit}USD"
+  budget_type       = "COST"
+  limit_amount      = var.budget_limit
+  limit_unit        = "USD"
+  time_period_start = timestamp() # "2022-03-05_00:00"
+  time_period_end   = timeadd(timestamp, var.budget_duration)
+  time_unit         = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = [var.email_address]
+  }
+}
