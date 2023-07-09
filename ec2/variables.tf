7ef1bb01326667ddfabaf6b3fd019ea5dd5b7aa9
Mateusz Umiński <17390661+mateusz-uminski@users.noreply.github.com>
2023-07-09 14:08:40 +0200

    feat(ec2): add module that creates an ec2 instance

---
+++ ec2/variables.tf
@@ -0,0 +1,68 @@
+variable "org_abbreviated_name" {
+  type        = string
+  description = "An abbreviated name of the organization."
+}
+
+variable "project" {
+  type        = string
+  description = "The name of the project to which the EC2 instance belongs."
+}
+
+variable "environment" {
+  type        = string
+  description = "The name of the environment to which the EC2 instance belongs."
+}
+
+variable "instance_name" {
+  type        = string
+  description = "The name of the EC2 instance."
+}
+
+variable "instance_type" {
+  type        = string
+  description = "The type of the EC2 instance."
+}
+
+variable "ami_name_pattern" {
+  type        = string
+  description = "The pattern for selecting AMI for the EC2 instance."
+}
+
+variable "key_pair" {
+  type        = string
+  description = "The key pair name used for."
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "The name of the VPC in which the EC2 instance will be launched."
+}
+
+variable "subnet_name" {
+  type        = string
+  description = "The name of the subnet in which the EC2 instance will be launched."
+}
+
+variable "assign_public_ip" {
+  type        = bool
+  description = "Indicates whether to assign a public IP address to the EC2 instance."
+  default     = false
+}
+
+variable "additional_security_groups" {
+  type        = list(string)
+  description = "A list of additional security group names to attach to the EC2 instance."
+  default     = []
+}
+
+variable "allowed_ingress_cidrs" {
+  type        = list(string)
+  description = "A list of CIDR blocks for allowed inbound traffic."
+  default     = []
+}
+
+variable "allowed_ingress_sgs" {
+  type        = list(string)
+  description = "A list of security group names for allowed inbound traffic."
+  default     = []
+}
