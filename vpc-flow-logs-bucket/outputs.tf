0507790e97e8aad4c23704e577b0343c66be7153
Mateusz Umiński <17390661+mateusz-uminski@users.noreply.github.com>
2023-07-09 13:34:35 +0200

    feat(vpc-flow-logs-bucket): return the name of the bucket for vpc flow logs

--- vpc-flow-logs-bucket/outputs.tf
+++ vpc-flow-logs-bucket/outputs.tf
@@ -2,3 +2,8 @@ output "arn" {
   value       = aws_s3_bucket.vpc_flow_logs.arn
   description = "The ARN of the created S3 bucket for VPC flow logs."
 }
+
+output "name" {
+  value       = aws_s3_bucket.vpc_flow_logs.id
+  description = "The name of the created S3 bucket for VPC flow logs."
+}
