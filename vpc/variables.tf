ecb2a900a607ebda954810d0ac281a36b70ba99a
Mateusz Umiński <17390661+mateusz-uminski@users.noreply.github.com>
2023-08-06 18:24:59 +0200

    fix(vpc): org_abbreviated_name description

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,6 +1,6 @@
 variable "org_abbreviated_name" {
   type        = string
-  description = "An abbreviated name of the organization, that is an owner of the budget."
+  description = "An abbreviated name of the organization."
 }
 
 variable "vpc_name" {
